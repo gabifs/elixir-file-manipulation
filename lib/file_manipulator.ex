8e4059360db4a47631d807d6093f0799899f4bfe
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-04-10 17:55:24 -0300

    implements create

--- lib/file_manipulator.ex
+++ lib/file_manipulator.ex
@@ -3,16 +3,17 @@ defmodule FileManipulator do
   Documentation for `FileManipulator`.
   """
 
-  @doc """
-  Hello world.
+  def create(file) do
+    with :ok <- File.mkdir(file.directory),
+         :ok <- File.write("#{file.directory}/#{file.name}", file.content) do
+      append_to_file(file)
+    end
+  end
 
-  ## Examples
-
-      iex> FileManipulator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp append_to_file(my_file) do
+    File.open("#{my_file.directory}/#{my_file.name}", [:read, :write], fn file ->
+      IO.read(file, :line)
+      IO.binwrite(file, my_file.content_to_append)
+    end)
   end
 end
